9362589cb8bf28e87025f73c9be769f78005672d
Philippe Négrel-Jerzy <negreljerzy.philippe@gmail.com>
2021-04-02 19:58:53 +0200

    feat: :sparkles: add quad loading algorithm

--- src/exemples_GL/cube/App.java
+++ src/exemples_GL/cube/App.java
@@ -2,10 +2,7 @@ import org.lwjgl.*;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
 import org.lwjgl.system.*;
-import org.lwjgl.assimp.AICamera.*;
 
-import java.io.PrintStream;
-import java.lang.Thread;
 
 import java.nio.*;
 
@@ -16,30 +13,46 @@ import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
 public class App {
-    
-    private long window;
+
+	// The window handle
+	private long window;
 
     // App loop settings
     private double secsPerUpdate = 1.0d / 30.0d; // interval between two ingame time ticks
     private double steps = 0.0;
 	private float loopSlot = 1f / 50; // game iteration length
 
-    // private FloatBuffer light_diffuse = BufferUtils.createFloatBuffer(4).put(new float[] {1.0, 0.0, 0.0, 1.0});  /* Red diffuse light. */
-    // private FloatBuffer light_position = BufferUtils.createFloatBuffer(4).put(new float[]{1.0, 1.0, 1.0, 0.0});  /* Infinite light location. */
-    // private double[][] n = new double[][]{  /* Normals for the 6 faces of a cube. */
-    // {-1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {1.0, 0.0, 0.0},
-    // {0.0, -1.0, 0.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, -1.0} };
-    // private int[][] faces = new int[][]{  /* Vertex indices for the 6 faces of a cube. */
-    // {0, 1, 2, 3}, {3, 2, 6, 7}, {7, 6, 5, 4},
-    // {4, 5, 1, 0}, {5, 6, 2, 1}, {7, 4, 0, 3} };
-    // private float[][] v = new float[8][3];  /* Will be filled in with X,Y,Z vertexes. */
+    // Cube mesh variables
+    private FloatBuffer light_diffuse = BufferUtils.createFloatBuffer(4).put(new float[] {1.0f, 0.0f, 0.0f, 1.0f}).flip();  /* Red diffuse light. */
+    private FloatBuffer light_position = BufferUtils.createFloatBuffer(4).put(new float[]{1.0f, 1.0f, 1.0f, 0.0f}).flip();  /* Infinite light location. */
+    private FloatBuffer[] n = new FloatBuffer[]{  /* Normals for the 6 faces of a cube. */
+                BufferUtils.createFloatBuffer(4).put(new float[] {-1.0f, 0.0f, 0.0f}).flip(), 
+                BufferUtils.createFloatBuffer(4).put(new float[] {0.0f, 1.0f, 0.0f}).flip(), 
+                BufferUtils.createFloatBuffer(4).put(new float[] {1.0f, 0.0f, 0.0f}).flip(),
+                BufferUtils.createFloatBuffer(4).put(new float[] {0.0f, -1.0f, 0.0f}).flip(), 
+                BufferUtils.createFloatBuffer(4).put(new float[] {0.0f, 0.0f, 1.0f}).flip(), 
+                BufferUtils.createFloatBuffer(4).put(new float[] {0.0f, 0.0f, -1.0f}).flip() 
+    };
+
+    // private IntBuffer[] faces = new IntBuffer[]{  /* Vertex indices for the 6 faces of a cube. */
+    //             BufferUtils.createIntBuffer(4).put(new int[] {0, 1, 2, 3}), 
+    //             BufferUtils.createIntBuffer(4).put(new int[] {3, 2, 6, 7}), 
+    //             BufferUtils.createIntBuffer(4).put(new int[] {7, 6, 5, 4}),
+    //             BufferUtils.createIntBuffer(4).put(new int[] {4, 5, 1, 0}), 
+    //             BufferUtils.createIntBuffer(4).put(new int[] {5, 6, 2, 1}), 
+    //             BufferUtils.createIntBuffer(4).put(new int[] {7, 4, 0, 3}) 
+    // };
+    int faces[][] = new int[][] {  /* Vertex indices for the 6 faces of a cube. */
+        {0, 1, 2, 3}, {3, 2, 6, 7}, {7, 6, 5, 4},
+        {4, 5, 1, 0}, {5, 6, 2, 1}, {7, 4, 0, 3} };
+    private FloatBuffer[] v = new FloatBuffer[8];  /* Will be filled in with X,Y,Z vertexes. */ // not sure about that syntax in java jwgl
+
 
 	public void run() {
 		System.out.println("Hello LWJGL " + Version.getVersion() + "!");
 
 		init();
-        loop();
-        
+		loop();
 
 		// Free the window callbacks and destroy the window
 		glfwFreeCallbacks(window);
@@ -53,9 +66,9 @@ public class App {
 	private void init() {
 		// Setup an error callback. The default implementation
 		// will print the error message in System.err.
-        try (GLFWErrorCallback glErr = GLFWErrorCallback.createPrint(System.err)) { // try-with-resources
-    		glErr.set();
-        }
+		GLFWErrorCallback glErr = GLFWErrorCallback.createPrint(System.err);
+        glErr.set();
+
 		// Initialize GLFW. Most GLFW functions will not work before doing this.
 		if ( !glfwInit() )
 			throw new IllegalStateException("Unable to initialize GLFW");
@@ -66,7 +79,7 @@ public class App {
 		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
 
 		// Create the window
-		window = glfwCreateWindow(1600, 900, "Test application", NULL, NULL);
+		window = glfwCreateWindow(1600, 900, "Hey there!", NULL, NULL);
 		if ( window == NULL )
 			throw new RuntimeException("Failed to create the GLFW window");
 
@@ -102,30 +115,50 @@ public class App {
 
 		// Make the window visible
 		glfwShowWindow(window);
+
 	}
 
-    /**
-     * old function coded back into the application for compatibility purposes
-     * (removed since 3.0)
-	 * - currently unused
-     * @param fovy
-     * @param aspect
-     * @param near
-     * @param far
-     */
-    private void gluPerspective(float fovy, float aspect, float near, float far) {
-        float bottom = -near * (float) Math.tan(fovy / 2);
-        float top = -bottom;
-        float left = aspect * bottom;
-        float right = -left;
-        glFrustum(left, right, bottom, top, near, far);
+    private void initScene() {
+        // Enable a single OpenGL light
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, new float[] {1.0f, 0.0f, 0.0f, 1.0f});
+        glLightfv(GL_LIGHT0, GL_POSITION, new float[] {1.0f, 1.0f, 1.0f, 0.0f});
+        glEnable(GL_LIGHT0);
+        glEnable(GL_LIGHTING);
+
+        glEnable(GL_DEPTH_TEST);
+        // glMatrixMode(GL_PROJECTION);
+        // // gluPerspective
+        glMatrixMode(GL_MODELVIEW);
+
+        glTranslatef(0.0f, 0.0f, -1.0f);
+        glRotatef(60f, 1.0f, 0.0f, 0.0f);
+        glRotatef(-20f, 0.0f, 0.0f, 1.0f);
     }
 
+    private void drawBox() {
+        
+        // for (int i = 0; i < 6; i++) {
+        //     glBegin(GL_QUADS);
+        //     glNormal3fv(n[i]);
+        //     glVertex3fv(v[faces[i][0]]);
+        //     glVertex3fv(v[faces[i][1]]);
+        //     glVertex3fv(v[faces[i][2]]);
+        //     glVertex3fv(v[faces[i][3]]);
+        //     glEnd();
+        // }
+        glBegin(GL_QUADS);
+        glNormal3fv(new float[] {-1.0f, -1.0f, 0.0f});
+        glVertex3fv(new float[] {1.0f, -1.0f, 0.0f});
+        glVertex3fv(new float[] {1.0f, 1.0f, 0.0f});
+        glVertex3fv(new float[] {-1.0f, 1.0f, 0.0f});
+        glEnd();
+    }
     /**
      * Main render routine
      */
     private void render() {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        drawBox();
 
         glfwSwapBuffers(window); // swap the color buffers
 
@@ -143,13 +176,13 @@ public class App {
         }
     }
 
+    /**
+     * Game state updating routine
+     */
     private void updateGameState() {
         // TODO : game state updating function
     }
 
-    /**
-     * Main loop
-     */
 	private void loop() {
 		// This line is critical for LWJGL's interoperation with GLFW's
 		// OpenGL context, or any context that is managed externally.
@@ -158,15 +191,17 @@ public class App {
 		// bindings available for use.
 		GL.createCapabilities();
 
+        initScene();
+
         double previous = glfwGetTime();
 
 		// Set the clear color
-		glClearColor(0.0f, 1.0f, 0.0f, 0.0f);
+		glClearColor(0.1f, 0.1f, 0.1f, 0.0f);
 
 		// Run the rendering loop until the user has attempted to close
 		// the window or has pressed the ESCAPE key.
 		while ( !glfwWindowShouldClose(window) ) {
-            double loopStartTime = glfwGetTime();
+			double loopStartTime = glfwGetTime();
             double elapsed = loopStartTime - previous; // time elapsed on the last frame (response time)
             previous = loopStartTime;
             steps += elapsed;
@@ -184,4 +219,5 @@ public class App {
 	public static void main(String[] args) {
 		new App().run();
 	}
+
 }
