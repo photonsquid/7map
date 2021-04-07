6dfe01475972f761904acc3e249f3ee6cbda7b3b
FaislX <79276882+FaislX@users.noreply.github.com>
2021-04-08 01:33:31 +0200

    Add files via upload

---
+++ src_data/Feature.java
@@ -0,0 +1,17 @@
+public class Feature{
+    private boolean displayName;
+    private String name;
+    private Point[] p;
+    private Types type;
+    private Category cat;
+    private String[] attribut;
+
+    public Feature(boolean displayName, String name, Point[] p, Types type, Category cat, String[] attribut) {
+        this.displayName = displayName;
+        this.name = name;
+        this.p = p;
+        this.type = type;
+        this.cat = cat;
+        this.attribut = attribut;
+    }
+}
---
+++ src_data/Types.java
@@ -0,0 +1,3 @@
+public enum Types{
+    Point, LineSring, Polygon;
+}
