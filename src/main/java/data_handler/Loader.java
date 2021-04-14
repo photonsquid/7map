b7a7dfbff9a44619b8a2941f6a7e79727619aead
seba12 <seba12.04@orange.fr>
2021-04-14 08:19:36 +0200

    fix(data): :bug: return an arrays of Chunks in the updateChunk method for linting

--- src/main/java/data_handler/Loader.java
+++ src/main/java/data_handler/Loader.java
@@ -2,6 +2,8 @@ package data_handler;
 
 public class Loader {
     public Chunk[] updateChunk(Point p1, Point p2) {
-        
+        Chunk chunks[]; 
+        chunks = new Chunk[20];
+        return chunks;
     }
 }
