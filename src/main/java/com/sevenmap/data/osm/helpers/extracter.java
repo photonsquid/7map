ebaac6e991fd7111f32bd0e7fcffc5537595317b
chap <seba12.04@orange.fr>
2021-05-10 13:06:25 +0200

    feat(xml-parser): :construction: implement a better parser

--- src/main/java/com/sevenmap/data/osm/helpers/extracter.java
+++ src/main/java/com/sevenmap/data/osm/helpers/extracter.java
@@ -1,62 +1,48 @@
 package com.sevenmap.data.osm.helpers;
 
-import java.lang.reflect.AnnotatedType;
 import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.function.Predicate;
 
 import com.sevenmap.data.osm.Node;
 
+import org.jdom.Element;
 import org.reflections.ReflectionUtils;
 
 public class extracter {
 
-  public static Node extractData(Iterator<?> i, Class<?> className) {
-    System.out.println(className.getName());
-    Set<Field> fields = ReflectionUtils.getAllFields(className, new Predicate() {
-      @Override
-      public boolean test(Object input) {
-        return true;
-      }
-    });
+  public static Node extractData(Element racine, Class<?> clazz) {
 
-    // Map<Field, Object> values = ...
-    for (Field f : fields) {
-      // f.setAccessible(true);
-      System.out.println(f.toString());
+    getAttribute(i, clazz);
 
-      // Nom de l'attribut
-      System.out.println(f.getName());
-
-      // Classe de l'attribut
-      System.out.println("--------------");
-      System.out.println("getAnnotatedType");
-      System.out.println("--------------");
-      AnnotatedType c = f.getAnnotatedType();
-      System.out.println(c.toString());
-
-      try {
-        Class<?> act = Class.forName(c.toString());
-        Set<Field> ff = ReflectionUtils.getAllFields(act, new Predicate() {
-          @Override
-          public boolean test(Object input) {
-            return true;
-          }
-        });
-
-        for (Field fff : ff) {
-          System.out.println(fff.toString());
-        }
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      }
+    while (i.hasNext()) {
 
     }
-    System.out.println("--------------");
-    System.out.println("--------------");
-    System.out.println("--------------");
+
     return new Node();
 
   }
+
+  private static HashMap<Field, Object> getAttribute(Iterator<?> i, Class<?> clazz) {
+    if (clazz.getClass().isAnnotationPresent(XMLClass.class)) {
+      Set<Field> fields = ReflectionUtils.getAllFields(clazz.getClass(), new Predicate() {
+        @Override
+        public boolean test(Object input) {
+          return true;
+        }
+      });
+      for (Field f : fields) {
+        System.out.println(f.getClass());
+        // getAttribute(i, f.getClass());
+      }
+      return new HashMap<Field, Object>();
+    } else {
+      System.out.println("coucou");
+      return new HashMap<Field, Object>();
+    }
+
+  }
+
 }
