4783b14531cd9b33c4c53a4fa9b3778549e65a39
Mmzhk21 <hamid.oukhnini2018@gmail.com>
2021-05-30 01:19:12 +0200

    appTest

--- src/test/java/com/sevenmap/AppTest.java
+++ src/test/java/com/sevenmap/AppTest.java
@@ -3,9 +3,11 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+
 /**
  * Unit test for simple App.
  */
+
 public class AppTest 
     extends TestCase
 {
