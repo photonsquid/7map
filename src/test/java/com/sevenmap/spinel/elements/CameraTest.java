35ad24fdac1849e520eb394797c60653a52b9765
Philippe Négrel-Jerzy <negreljerzy.philippe@gmail.com>
2021-05-30 04:12:42 +0200

    fix(rendering): :white_check_mark: update faulty tests

--- src/test/java/com/sevenmap/spinel/elements/CameraTest.java
+++ src/test/java/com/sevenmap/spinel/elements/CameraTest.java
@@ -2,7 +2,7 @@ package com.sevenmap.spinel.elements;
 
 import static org.junit.Assert.assertEquals;
 
-//import com.sevenmap.spinel.elements.Camera;
+import com.sevenmap.spinel.elements.Camera;
 import com.sevenmap.spinel.math.Vector3f;
 
 import org.junit.Before;
@@ -14,10 +14,9 @@ public class CameraTest {
     private Camera cam1;
     private Vector3f pos0, rot0, pos1, rot1;
 
-
     @Before
     public void setUp() {
-        
+
         // Declaring vectors
         pos0 = new Vector3f(3.5f, 2.5f, 1.5f);
         rot0 = new Vector3f(2.0f, 3.0f, 4.0f);
@@ -25,12 +24,12 @@ public class CameraTest {
         rot1 = new Vector3f(7.0f, 0.5f, 6.1f);
 
         // Declaring Cameras
-        cam0 = new Camera(pos0 , rot0, 1920f/1080f);
-        cam1 = new Camera(pos1 , rot1, 1920f/1080f);
+        cam0 = new Camera(pos0, rot0, 1920f / 1080f);
+        cam1 = new Camera(pos1, rot1, 1920f / 1080f);
     }
-    
+
     @Test
-    public void testPos(){
+    public void testPos() {
         assertEquals(cam0.getPos(), pos0);
         assertEquals(cam1.getPos(), pos1);
     }
@@ -40,5 +39,5 @@ public class CameraTest {
         assertEquals(cam1.getRot(), rot1);
         assertEquals(cam0.getRot(), rot0);
     }
-    
+
 }
